1d6d17b45b6201bde5607925f2fa418c265e202d
Ansh Mehta <ansh.mehta@somaiya.edu>
2020-07-21 17:17:07 +0530

    Updated REQUIREMENTS and SETTINGS

--- HouseHelpers/settings.py
+++ HouseHelpers/settings.py
@@ -129,6 +129,11 @@ STATIC_ROOT= os.path.join(BASE_DIR, 'staticfiles'),
 STATIC_URL = '/static/'
 
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
 EMAIL_USE_TLS=True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER=os.environ.get('EMAIL_USER')
