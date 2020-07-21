194f78d7ba2dc9f5f3764e3abf7862213c677cb1
Ansh Mehta <ansh.mehta@somaiya.edu>
2020-07-21 18:02:54 +0530

    Updated SECRET_KEYS and ENVIRONMENT VARIABLES

--- HouseHelpers/settings.py
+++ HouseHelpers/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#wp#^2ghhmp)j63q15al_r*=l1j5m782knv55g-!&6@94t8$&*'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['househelpers.herokuapp.com']
 
 
 # Application definition
@@ -139,5 +139,3 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER=os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_PASSWORD')
 EMAIL_PORT=587
-# EMAIL_HOST_USER=
-# EMAIL_HOST_PASSWORD=
