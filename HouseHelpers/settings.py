7afa066c6a8f587567f7defe5fadac7e88c7c823
Ansh Mehta <ansh.mehta@somaiya.edu>
2020-07-22 19:06:25 +0530

    REMOVED EMAIL VERIFICATION

--- HouseHelpers/settings.py
+++ HouseHelpers/settings.py
@@ -87,9 +87,9 @@ DATABASES = {
     }
 }
 
-DATABASE_URL = os.environ['DATABASE_URL']
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+# DATABASE_URL = os.environ['DATABASE_URL']
+# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+# DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
